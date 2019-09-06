a30bf981b8eb3b91f1e00d76f3373dfa58ecbdbc
ezmaster <edalov.ivan@yandex.ru>
2019-09-06 15:27:42 +0300

    player possesing

--- bt/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ bt/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,9 +3,25 @@
 
 #include "TankPlayerController.h"
 
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 
-
 	return Cast<ATank>(GetPawn());
+
 }
